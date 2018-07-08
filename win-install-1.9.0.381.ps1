e8da16a226fa59ee3c8bf9b9caa2c715d48b1f4a
michael frericks <michael-frericks@web.de>
2018-07-08 15:58:59 +0200

    handling downloaded file and temp dirs when reruninng the script

--- win-install-1.9.0.381.ps1
+++ win-install-1.9.0.381.ps1
@@ -1,3 +1,5 @@
+# define parameters
+
 param (
  [string]
     [alias("p")]
@@ -6,18 +8,11 @@ param (
    
     )
 # Start
-function do-usage {
-echo "Installs the Clojure command line tools."
-echo "Usage:"
-  echo "win-install-1.9.0.381.ps1[-p|--prefix <dir>]"
-  exit 1
-}
 
-echo "Downloading and expanding tar"
+# define vars for download and extracting
 $url = "https://download.clojure.org/install/clojure-tools-1.9.0.381.tar.gz"
-$file1= "clojure-tools-1.9.0.381.tar.gz"
-$file2= "clojure-tools-1.9.0.381.tar"
-$output = "$PSScriptRoot\clojure-tools-1.9.0.381.tar.gz"
+$clj_tools_archive_file_tar_gz = "$PSScriptRoot\clojure-tools-1.9.0.381.tar.gz"
+$clj_tools_archive_file_tar= "clojure-tools-1.9.0.381.tar"
 
 $install_dir=$prefix_dir + "\clojure"
 $lib_dir=$install_dir + "/lib"
@@ -27,15 +22,25 @@ $man_dir="$prefix_dir/share/man/man1"
 $clojure_lib_dir="$lib_dir/clojure"
 
 
-Remove-Item $file1
-Remove-Item $file2
+function do-usage {
+echo "Installs the Clojure command line tools."
+echo "Usage:"
+  echo "win-install-1.9.0.381.ps1[-p|--prefix <dir>]"
+  exit 1
+}
+function cleanup {
+# remove  old download files and temp dirs
+Remove-Item $clj_tools_archive_file_tar_gz
+Remove-Item $clj_tools_archive_file_tar
 Remove-Item .\clojure-tools -Force -Recurse
-Remove-Item $install_dir -Force -Recurse
+
+}
+
 echo downloading $url  ...
 $start_time = Get-Date
 
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $output)
+$wc.DownloadFile($url, $clj_tools_archive_file_tar_gz)
 
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
@@ -43,10 +48,15 @@ Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
 $env:Path = $env:Path + ";C:\Program Files\7-zip"
 
-7z.exe  x "clojure-tools-1.9.0.381.tar.gz"
+7z.exe  x $clj_tools_archive_file_tar_gz
+
 7z.exe  x "clojure-tools-1.9.0.381.tar"
 
 
+# cleanup install dir
+Get-Item  $install_dir  | Remove-Item  -Force -Recurse
+
+# prepare install dir 
 New-Item -Path $install_dir -ItemType "directory"
 New-Item -Path $lib_dir  -ItemType "directory"
 New-Item -Path $lib_exec   -ItemType "directory"
@@ -65,8 +75,10 @@ echo "Installing man pages into $man_dir"
 install -Dm644 clojure-tools/clojure.1 "$man_dir/clojure.1"
 install -Dm644 clojure-tools/clj.1 "$man_dir/clj.1"
 
-echo "Removing download"
-rm -rf clojure-tools
-rm -rf clojure-tools-1.9.0.381.tar.gz
+
+cleanup
+#echo "Removing download"
+#Remove-Item $clj_tools_archive_file_tar_gz
+
 
 echo "Use clj -h for help."
