7881bf3015989be078566438f989b28848792287
Michael Frericks <Michael-Frericks@web.de>
2020-09-26 22:07:32 +0200

    update usage string

--- clojure/clojure.go
+++ clojure/clojure.go
@@ -83,39 +83,58 @@ func copy(src string, dest string) {
 
 func usage(version string) {
 	fmt.Printf("Version: %s\n", version)	
-	fmt.Println(` 	Usage: clojure [dep-opt*] [--] [init-opt*] [main-opt] [arg*]
-	       clj     [dep-opt*] [init-opt*] [--] [main-opt] [arg*]
+	fmt.Println(`You use the Clojure tools ('clj' or 'clojure') to run Clojure programs
+	on the JVM, e.g. to start a REPL or invoke a specific function with data.
+	The Clojure tools will configure the JVM process by defining a classpath
+	(of desired libraries), an execution environment (JVM options) and
+	specifying a main class and args. 
 
-	The clojure script is a runner for Clojure. clj is a wrapper
-	for interactive repl use. These scripts ultimately construct and
-	invoke a command-line of the form:
+	Using a deps.edn file (or files), you tell Clojure where your source code
+	resides and what libraries you need. Clojure will then calculate the full
+	set of required libraries and a classpath, caching expensive parts of this
+	process for better performance.
 
-	java [java-opt*] -cp classpath clojure.main [init-opt*] [main-opt] [arg*]
+	The internal steps of the Clojure tools, as well as the Clojure functions
+	you intend to run, are parameterized by data structures, often maps. Shell
+	command lines are not optimized for passing nested data, so instead you
+	will put the data structures in your deps.edn file and refer to them on the
+	command line via 'aliases' - keywords that name data structures.
 
-	The dep-opts are used to build the java-opts and classpath:
+	'clj' and 'clojure' differ in that 'clj' has extra support for use as a REPL
+	in a terminal, and should be preferred unless you don't want that support,
+	then use 'clojure'.
+
+	Usage:
+	  Start a REPL   clj     [clj-opt*] [-A:aliases] [init-opt*]
+	  Exec function  clojure [clj-opt*] -X[:aliases] [a/fn] [kpath v]*
+	  Run main       clojure [clj-opt*] -M[:aliases] [init-opt*] [main-opt] [arg*]
+	  Prepare        clojure [clj-opt*] -P [other exec opts]
+
+	exec-opts:
+	 -A:aliases     Use aliases to modify classpath
+	 -X[:aliases]   Use aliases to modify classpath or supply exec fn/args
+	 -M[:aliases]   Use aliases to modify classpath or supply main opts
+	 -P             Prepare deps - download libs, cache classpath, but don't exec
+
+	clj-opts:
 	 -Jopt          Pass opt through in java_opts, ex: -J-Xmx512m
-	 -Oalias...     Concatenated jvm option aliases, ex: -O:mem
-	 -Ralias...     Concatenated resolve-deps aliases, ex: -R:bench:1.9
-	 -Calias...     Concatenated make-classpath aliases, ex: -C:dev
-	 -Malias...     Concatenated main option aliases, ex: -M:test
-	 -Aalias...     Concatenated aliases of any kind, ex: -A:dev:mem
- 	 -Sdeps EDN     Deps data to use as the last deps file to be merged
+	 -Sdeps EDN     Deps data to use as the last deps file to be merged
 	 -Spath         Compute classpath and echo to stdout only
+	 -Spom          Generate (or update) pom.xml with deps and paths
+	 -Stree         Print dependency tree
 	 -Scp CP        Do NOT compute or cache classpath, use this one instead
 	 -Srepro        Ignore the ~/.clojure/deps.edn config file
 	 -Sforce        Force recomputation of the classpath (don't use the cache)
-	 -Spom          Generate (or update existing) pom.xml with deps and paths
-	 -Stree         Print dependency tree
-	 -Sresolve-tags Resolve git coordinate tags to shas and update deps.edn
 	 -Sverbose      Print important path info to console
 	 -Sdescribe     Print environment and command parsing info as data
-         -Sthreads      Set specific number of download threads
-         -Strace        Write a trace.edn file that traces deps expansion
-         --             Stop parsing dep options and pass remaining arguments to clojure.main
+	 -Sthreads      Set specific number of download threads
+	 -Strace        Write a trace.edn file that traces deps expansion
+	 --             Stop parsing dep options and pass remaining arguments to clojure.main
 
 	init-opt:
 	 -i, --init path     Load a file or resource
 	 -e, --eval string   Eval exprs in string; print non-nil values
+	 --report target     Report uncaught exception to "file" (default), "stderr", or "none"
 
 	main-opt:
 	 -m, --main ns-name  Call the -main function from namespace w/args
@@ -124,11 +143,15 @@ func usage(version string) {
 	 -                   Run a script from standard input
 	 -h, -?, --help      Print this help message and exit
 
+	Programs provided by :deps alias:
+	 -X:deps mvn-install       Install a maven jar to the local repository cache
+	 -X:deps git-resolve-tags  Resolve git coord tags to shas and update deps.edn
+
 	For more info, see:
 	 https://clojure.org/guides/deps_and_cli
 	 https://clojure.org/reference/repl_and_main
 `)
-
+	
 }
 
 func main() {
